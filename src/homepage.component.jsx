f17ea9e1fec5007cb1ecb200799ad718375f22cb
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2020-04-16 17:13:51 +0100

    installed node-sass as dependency

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -11,32 +11,28 @@ const HomePage = () => {
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
+
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">JACKETS</h1>
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
+
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">SNEAKERS</h1>
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
+
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">WOMENS</h1>
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
+
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">MENS</h1>
