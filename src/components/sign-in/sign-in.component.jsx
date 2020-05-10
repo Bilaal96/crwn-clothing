3948c709977660c3185abf40eaac1d81c82ee13c
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2020-05-10 20:18:57 +0100

    small fix to Google Signin Button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -66,7 +66,11 @@ class SignIn extends React.Component {
                     />
                     <div className="buttons">
                         <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton
+                            type="button"
+                            onClick={signInWithGoogle}
+                            isGoogleSignIn
+                        >
                             Sign In With Google
                         </CustomButton>
                     </div>
