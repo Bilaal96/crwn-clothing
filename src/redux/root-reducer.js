a66d02d255f9ebc53a763526236cd29528afbc57
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2020-08-31 17:56:12 +0100

    Installed redux-persist Configured store & root-reducer to persist state to localStorage In index.js, Wrapped App component with PersistGate component to provide app with context of persistReducer Modified .sign-in-link colors when it is selected

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,31 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// Local Storage Object on Browser Window => default for web
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+/**
+ * --- Redux Persist Congiguration Options ---
+ * key => initial position where we want to store data in our persistReducer
+ 
+ * storage => the storage method/type specified with Redux persist => storage defaults to localStorage for web
+ 
+ * whitelist => array containing string names of any reducers we want to store & persist
+ */
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['cart'],
+};
+
+// rootReducer
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+
+// Returns & exports persistedReducer
+// --> rootReducer modified with persistConfig options
+export default persistReducer(persistConfig, rootReducer);
